1e13a5decc322b66f3c3b7b1dd6439a45bf7bd6d
Simon Baechler <sb@feinheit.ch>
2015-05-29 19:46:40 +0200

    add tox tests

--- setup.py
+++ setup.py
@@ -5,6 +5,16 @@ import os
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+# Hard linking doesn't work inside VirtualBox shared folders. This means that
+# you can't use tox in a directory that is being shared with Vagrant,
+# since tox relies on `python setup.py sdist` which uses hard links. As a
+# workaround, disable hard-linking if setup.py is a descendant of /vagrant.
+# See
+# https://stackoverflow.com/questions/7719380/python-setup-py-sdist-error-operation-not-permitted
+# for more details.
+if os.path.abspath(__file__).split(os.path.sep)[1] == 'vagrant':
+    del os.link
+
 setup(
     name='django-multilingual-search',
     version='2.3.0.alpha',
@@ -17,11 +27,11 @@ setup(
                 'multilingual indexes for Django.',
     long_description=read('README.md'),
     platforms=['OS Independent'],
-    install_requires=[
-        'Django>=1.4',
-        'haystack>=2.3.1',
-        'elasticsearch=>1.5.0'
-    ],
+    # install_requires=[
+    #     'Django>=1.4',
+    #     'haystack>=2.3.1',
+    #     'elasticsearch=>1.5.0'
+    # ],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: BSD License',
---
+++ tests/testproject/tests.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import
+
+from tests.unittests.test_index_creation import BackendTest as BT1
+from tests.unittests.test_query import BackendTest as BT2
+from tests.functional_tests.test_query import BackendTest as BT3
+from tests.functional_tests.test_index_creation import IndexTest
