92a323c8a1ac18c04a8822155ad534226fe1a233
Simon Bächler <simon@stellanera.com>
2015-10-31 23:29:32 +0100

    use haystack 2.4.1

--- tests/functional_tests/test_query.py
+++ tests/functional_tests/test_query.py
@@ -32,7 +32,7 @@ class BackendTest(TestCase):
         if self.index_exists:
             engine = ElasticsearchMultilingualSearchEngine()
             es = engine.backend('default', **Data.connection_options)
-            es.clear(commit=True)
+            es.clear(models=None, commit=True)
             time.sleep(1)
 
     def test_live_query(self):
@@ -70,10 +70,10 @@ class BackendTest(TestCase):
             self.assertEqual(self.count, count['count'])
 
             # make sure the index has been created
-            while not es.conn.exists(index=index_name, id=id) and i < 5:
+            while not es.conn.exists(index=index_name, doc_type='_all', id=id) and i < 5:
                 time.sleep(0.5)
                 i += 1
-            self.assertTrue(es.conn.exists(index=index_name, id=id))
+            self.assertTrue(es.conn.exists(index=index_name, doc_type='_all', id=id))
             # get the document with the above id from the correct index
             doc = es.conn.get(index=index_name, id=id)
             self.assertTrue(doc['found'])
@@ -130,10 +130,10 @@ class BackendTest(TestCase):
             self.assertEqual(self.count, count['count'])
 
             # make sure the index has been created
-            while not es.conn.exists(index=index_name, id=id) and i < 5:
+            while not es.conn.exists(index=index_name, doc_type='_all', id=id) and i < 5:
                 time.sleep(0.5)
                 i += 1
-            self.assertTrue(es.conn.exists(index=index_name, id=id))
+            self.assertTrue(es.conn.exists(index=index_name, doc_type='_all', id=id))
             # get the document with the above id from the correct index
             doc = es.conn.get(index=index_name, id=id)
             self.assertTrue(doc['found'])
